439751a5ccd9f12fb5c5ec8512b42f880ec4f7af
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-07 13:57:50 -0300

    refactor: remove unused code and comments from AppServiceProvider, ShortLinkResource, and User models

--- app/Http/Resources/ShortLinkResource.php
+++ app/Http/Resources/ShortLinkResource.php
@@ -8,19 +8,3 @@ use App\Models\ShortLink;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin ShortLink */
-final class ShortLinkResource extends JsonResource
-{
-    public function toArray(Request $request): array
-    {
-        return [
-            'id'           => $this->id_hashed,
-            'endpoint'     => $this->endpoint,
-            'url_redirect' => url($this->slug ?: $this->id_hashed),
-            'slug'         => $this->slug,
-            'user_id'      => $this->user_id_hashed,
-            'created_at'   => $this->created_at,
-            'updated_at'   => $this->updated_at,
-        ];
-    }
-}
