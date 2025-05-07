8584028f2f8bebd9301b38310201ec38a7082220
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-07 14:36:04 -0300

    refactor: apply coding standards and add strict types to various files

--- app/Models/ShortLinkClick.php
+++ app/Models/ShortLinkClick.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ShortLinkClick extends Model
+final class ShortLinkClick extends Model
 {
     protected $fillable = [
         'ip_address',
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ShortLinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
+Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function (): void {
     Route::apiResource('short-links', ShortLinkController::class)
