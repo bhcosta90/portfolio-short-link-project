d6af65d22f8e36a3ef09ff32267ff01d0e4588bf
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 11:11:19 -0300

    feat: add geoIp relationship to ShortLinkClick model

--- app/Models/ShortLinkClick.php
+++ app/Models/ShortLinkClick.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 final class ShortLinkClick extends Model
 {
@@ -12,4 +13,9 @@ final class ShortLinkClick extends Model
         'ip_address',
         'endpoint',
     ];
+
+    public function geoIp(): HasOne
+    {
+        return $this->hasOne(GeoIp::class, 'ip_address', 'ip_address');
+    }
 }
