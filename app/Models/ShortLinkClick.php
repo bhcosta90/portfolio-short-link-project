1844e8759d641a6f02e257233906578241b8b45c
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 11:15:02 -0300

    feat: apply AsHashed trait to ShortLinkClick model for improved data handling

--- app/Models/ShortLinkClick.php
+++ app/Models/ShortLinkClick.php
@@ -4,11 +4,13 @@ declare(strict_types = 1);
 
 namespace App\Models;
 
+use App\Models\Traits\AsHashed;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 final class ShortLinkClick extends Model
 {
+    use AsHashed;
     protected $fillable = [
         'ip_address',
         'endpoint',
