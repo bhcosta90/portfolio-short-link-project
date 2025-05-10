83a14b1265f9f98238c2ac554dc1e48c95422325
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 09:29:41 -0300

    feat: add AddClickOnShortLinkListener to handle short link click events

---
+++ app/Listeners/AddClickOnShortLinkListener.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Listeners;
+
+use App\Events\RegisterClickShortLinkEvent;
+use App\Models\ShortLink;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+final class AddClickOnShortLinkListener implements ShouldQueue
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(RegisterClickShortLinkEvent $event): void
+    {
+        $shortLink = ShortLink::query()->findOrFail($event->id);
+
+        $shortLink?->shortLinkClick()->create([
+            'ip_address' => $event->ipAddress,
+            'endpoint'   => $event->endpoint,
+        ]);
+    }
+}
