33fad43848fcfd390afb90991f835375a83df3bd
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 09:24:24 -0300

    feat: add RegisterClickShortLinkEvent for handling short link click events

---
+++ app/Events/RegisterClickShortLinkEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+
+final class RegisterClickShortLinkEvent
+{
+    use Dispatchable;
+
+    public function __construct(
+        public int $id,
+        public string $endpoint,
+        public string $ipAddress,
+    ) {
+    }
+}
