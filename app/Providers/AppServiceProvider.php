6ee5131fe924b11b075488190008f8bae257db27
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 12:32:16 -0300

    refactor: move Http::preventStrayRequests() to TestCase setup method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -15,8 +14,6 @@ final class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
-
-        Http::preventStrayRequests();
     }
 
     public function boot(): void
