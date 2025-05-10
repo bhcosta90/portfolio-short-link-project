b91ad5e6af8bd55cd393f7c4f5edfef9fd55b927
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-05-10 09:29:37 -0300

    feat: add endpoint column to short_link_clicks table

--- database/migrations/2025_05_07_173127_create_short_link_clicks_table.php
+++ database/migrations/2025_05_07_173127_create_short_link_clicks_table.php
@@ -11,6 +11,7 @@ return new class() extends Migration {
     {
         Schema::create('short_link_clicks', function (Blueprint $table): void {
             $table->id();
+            $table->string('endpoint');
             $table->string('ip_address');
             $table->timestamps();
         });
