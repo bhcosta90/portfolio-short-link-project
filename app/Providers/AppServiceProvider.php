346bbd2d360e73dd02ad89e5eb742b9997c7e488
Bruno Costa <bhcosta90@gmail.com>
2025-05-15 16:35:57 -0300

    feat: enhance AppServiceProvider with multiple configuration methods for improved application setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,57 @@ final class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
+
+        $this->configureUrl();
+        $this->configureModel();
+        $this->configureTests();
+        $this->configurePassword();
+        $this->configureDB();
+        $this->configureVite();
+        $this->configureDate();
     }
 
     public function boot(): void
     {
         //
+    }
+
+    protected function configureUrl(): void
+    {
+        URL::forceHttps(app()->isProduction());
+    }
+
+    protected function configureModel(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    protected function configureTests(): void
+    {
+        Http::preventStrayRequests(!app()->isProduction());
+    }
+
+    protected function configurePassword(): void
+    {
+        if (app()->isProduction()) {
+            Password::defaults(fn () => Password::min(12)->max(100)->uncompromised());
+        }
+    }
+
+    protected function configureDB(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    protected function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    protected function configureDate(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
